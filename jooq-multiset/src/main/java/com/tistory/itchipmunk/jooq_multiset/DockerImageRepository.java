b66dc913baebb38503003ba6bc0c401c2a16d1b6
kor-Chipmunk <rhj4862@gmail.com>
2025-03-20 02:57:29 +0900

    feat: 예제 레코드 생성하는 insert 코드 추가

--- jooq-multiset/src/main/java/com/tistory/itchipmunk/jooq_multiset/DockerImageRepository.java
+++ jooq-multiset/src/main/java/com/tistory/itchipmunk/jooq_multiset/DockerImageRepository.java
@@ -6,6 +6,9 @@ import static org.jooq.impl.DSL.multiset;
 import static org.jooq.impl.DSL.select;
 
 import com.tistory.itchipmunk.jooq_multiset.DockerImageModel.DockerImageTagModel;
+import com.tistory.itchipmunk.jooq_multiset.tables.records.DockerImageTagsRecord;
+import com.tistory.itchipmunk.jooq_multiset.tables.records.DockerImagesRecord;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,21 @@ import org.springframework.stereotype.Repository;
 public class DockerImageRepository {
 
     private final DSLContext dsl;
+
+    // 예시 레코드 생성
+    public void insert() {
+        List<DockerImagesRecord> records = new ArrayList<>(1_000_000);
+        List<DockerImageTagsRecord> tagRecords = new ArrayList<>(10_000_000);
+        for (int i = 2; i < 1_000_000; i++) {
+            records.add(new DockerImagesRecord((long) i, "name" + i, "description" + i));
+
+            for (int j = 0; j < 10; j++) {
+                tagRecords.add(new DockerImageTagsRecord( null, (long) i, "name" + j ));
+            }
+        }
+        dsl.batchInsert(records).execute();
+        dsl.batchInsert(tagRecords).execute();
+    }
 
     public List<DockerImageModel> getDockerImages() {
         var fetched = dsl.select(
